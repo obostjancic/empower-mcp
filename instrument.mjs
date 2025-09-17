109cf91eef3a97d46f08d1cf623f4f8360113f94
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-09-17 10:35:38 +0200

    update DSN

--- instrument.mjs
+++ instrument.mjs
@@ -1,10 +1,7 @@
 import * as Sentry from "@sentry/node";
-// const Sentry = require("@sentry/node");
 
 Sentry.init({
-  dsn: "https://9014a182b36f57011af3b20f927d179c@o4509089275772928.ingest.de.sentry.io/4510028461703248",
-  // Setting this option to true will send default PII data to Sentry.
-  // For example, automatic IP address collection on events
+  dsn: "https://e9eb3b0ff1e7a727244e1abf8e9574ab@sandbox-mirror.sentry.gg/1",
   sendDefaultPii: true,
   tracesSampleRate: 1.0,
 });
